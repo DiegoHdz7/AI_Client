00b47f6ad1a6decfabf86c2842e724888684c499
josekg <efrenhdez9812@gmail.com>
2023-12-10 22:13:17 -0500

    Tumor page

--- server/app.py
+++ server/app.py
@@ -23,29 +23,72 @@ model_columns_tumors = joblib.load('tumors_model_columns.pkl')
 @app.route('/predict/tumors', methods=['POST'])
 def predictTumor():
     try:
+        # # Get JSON input from the request
+        # input_data = request.get_json()
+
+        # # Create a DataFrame from the input data
+        # input_df = pd.DataFrame([input_data], columns=model_columns_tumors)
+
+        #  # Ensure that the input_df has the same columns as the model_columns
+        # input_df = input_df[model_columns_tumors]
+    
+        #  # Create MinMaxScaler and scale the features
+        # minmax_scale = MinMaxScaler()
+        # input_scaled = minmax_scale.fit_transform(input_df)
+    
+        #  # Make predictions
+        # predictions = model_tumors.predict(input_scaled)
+        # user_predicted_label = np.argmax(predictions)
+        #  # Map numerical labels to class names
+        # class_names = {0: 'LGG', 1: 'GBM'}
+        # predicted_class = class_names[user_predicted_label]
+    
+        # # Return the predicted class as JSON
+        # return {'prediction': predicted_class,
+        #                  'predictions':predictions.tolist()}
+        
         # Get JSON input from the request
         input_data = request.get_json()
+
+        # Print the received input data for debugging
+        print('Received Data:')
+        print(input_data)
 
         # Create a DataFrame from the input data
         input_df = pd.DataFrame([input_data], columns=model_columns_tumors)
 
-         # Ensure that the input_df has the same columns as the model_columns
+        # Ensure that the input_df has the same columns as the model_columns
         input_df = input_df[model_columns_tumors]
-    
-         # Create MinMaxScaler and scale the features
+
+        # Print the processed input data for debugging
+        print('Processed Input Data:')
+        print(input_df)
+
+        # Create MinMaxScaler and scale the features
         minmax_scale = MinMaxScaler()
         input_scaled = minmax_scale.fit_transform(input_df)
-    
-         # Make predictions
+
+        # Print the scaled input data for debugging
+        print('Scaled Input:')
+        print(input_scaled)
+
+        # Make predictions
         predictions = model_tumors.predict(input_scaled)
         user_predicted_label = np.argmax(predictions)
-         # Map numerical labels to class names
+
+        # Map numerical labels to class names
         class_names = {0: 'LGG', 1: 'GBM'}
         predicted_class = class_names[user_predicted_label]
-    
+
+        # Print predictions for debugging
+        print('Prediction Label:')
+        print(predicted_class)
+        print('Predictions:')
+        print(predictions)
+
         # Return the predicted class as JSON
         return {'prediction': predicted_class,
-                         'predictions':predictions.tolist()}
+                'predictions': predictions.tolist()}
               
 
 
